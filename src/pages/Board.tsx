4ae35d238fe2eeb3fa8ce1e3350827ec3e12f948
mrmckney <mrmckney@gmail.com>
2022-11-30 01:03:09 -0500

    displayed countries and added format for population

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import '../css/board.css'
 
 interface newCountry {
@@ -20,9 +20,10 @@ const Board = () => {
 
     const [gameStarted, setGameStarted] = useState<boolean>(false)
     const [playerCountry, setPlayerCountry] = useState<newCountry>({name: '', flag: '', population: 0})
+    const [playerPopulation, setPlayerPopulation] = useState<number>(0)
     const [botCountry, setBotCountry] = useState<newCountry>({name: '', flag: '', population: 0})
+    const [botPopulation, setBotPopulation] = useState<number>(0)
  
-    console.log(playerCountry)
     const grabCountries = async () => {
         const countries = await fetch('https://api.sampleapis.com/countries/countries')
         const data = await countries.json()
@@ -46,38 +47,66 @@ const Board = () => {
         const allCountries = await grabCountries()
         const randomCountries = await getRandomCountries(allCountries)
         setPlayerCountry(randomCountries[0])
+        setPlayerPopulation(randomCountries[0].population)
         setBotCountry(randomCountries[1])
+        setBotPopulation(randomCountries[1].population)
         setGameStarted(true)
     }
     
 
     return (
         <div>
+            <div className="title">
+                <h1>Player Country</h1>
+                <h1>Bot Country</h1>
+            </div>
             <div className="board">
-                <div>
-                    <h2>{playerCountry.name}</h2>
-                    <img className="flag" src={playerCountry.flag} alt="Countries Flag"/>
-                    <h3>Population</h3>
-                    <h4>{new Intl.NumberFormat().format(playerCountry.population)}</h4>
-                </div>
-                <div>
-                    <h2>{botCountry.name}</h2>
-                    <img className="flag" src={botCountry.flag} alt="Countries Flag"/>
-                    <h3>Population</h3>
-                    <h4>{new Intl.NumberFormat().format(botCountry.population)}</h4>
-                </div>
+                {playerCountry.name != '' ? 
+                    <div className="box">
+                        <h2>{playerCountry.name}</h2>
+                        <img className="flag" src={playerCountry.flag} alt="Countries Flag"/>
+                        <h3>Population</h3>
+                        <h4>{new Intl.NumberFormat().format(playerPopulation)}</h4>
+                    </div>
+                    :
+                    <div className="box">
+                        <h1>Placeholder</h1>
+                    </div>
+                
+                }
+                {botCountry.name != '' ? 
+                    <div className="box">
+                        <h2>{botCountry.name}</h2>
+                        <img className="flag" src={botCountry.flag} alt="Countries Flag"/>
+                        <h3>Population</h3>
+                        <h4>{new Intl.NumberFormat().format(botPopulation)}</h4>
+                    </div>
+                    :
+                    <div className="box">
+                        <h1>Placeholder</h1>
+                    </div>
+                
+                }
             </div>
-            {gameStarted ? 
-            <div>
-                <button>Attack</button>
-                <button>Stop</button>
-                <button>Defend</button>
-            </div>
-            : 
-            <></>
+            {gameStarted 
+                ? 
+                <div>
+                    <div>
+                        <button onClick={() => setBotPopulation(botPopulation - 50000)}>Attack</button>
+                        <button>Defend</button>
+                    </div>
+                    <button onClick={() => setGameStarted(false)}>Stop</button>
+                </div>
+                : 
+                <></>
         
-        }
-            <h3 onClick={() => gameStart()}>Generate Game</h3>
+            }
+            {!gameStarted 
+                ? 
+                <h3 onClick={() => gameStart()}>Generate Game</h3>
+                :
+                <></>
+            }
         </div>
     )
 }
